107c509cce12bb08099e173dd69ccb150aea59f4
nam977 <nam@njit.edu>
2025-10-05 23:46:23 -0400

    added json test files to test input in cmdline

---
+++ jsontest4.php
@@ -0,0 +1,14 @@
+<?php
+$myjson_input = array(
+	"type" => "register",
+	"username" => "fakepassword",
+	"password" => "fakepassword",
+	"message" => "test message"
+);
+
+$myjson_input = json_encode($myjson_input);
+
+exec("./testRabbitMQClient.php jsonname '$myjson_input'", $output, $return_var);
+print_r($output);
+print_r($return_var);
+print_r($myjson_input);
